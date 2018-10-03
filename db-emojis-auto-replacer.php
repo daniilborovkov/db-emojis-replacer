94e3419f7e76ed305e2afb5391a040c8e5b14915
daniilborovkov <daniilborowkow@gmail.com>
2018-10-04 01:36:16 +0600

    Initial commit

---
+++ db-emojis-auto-replacer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Db Emojis Auto Replacer
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     db-emojis-auto-replacer
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Db_Emojis_Auto_Replacer
+ */
+
+// Your code starts here.
