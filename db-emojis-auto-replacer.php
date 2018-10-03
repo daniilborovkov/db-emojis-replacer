e7b5ed6f8a40712f7c50a6524db116023d8a45a3
daniilborovkov <daniilborowkow@gmail.com>
2018-10-04 01:37:42 +0600

    Added credentials

--- db-emojis-auto-replacer.php
+++ db-emojis-auto-replacer.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * Plugin Name:     Db Emojis Auto Replacer
- * Plugin URI:      PLUGIN SITE HERE
- * Description:     PLUGIN DESCRIPTION HERE
- * Author:          YOUR NAME HERE
- * Author URI:      YOUR SITE HERE
+ * Plugin URI:      daniilborovkov.github.io/db-emojis-autoreplacer
+ * Description:     Small shortcoded plugin for autoreplacer
+ * Author:          Daniil Borovkov
+ * Author URI:      daniilborovkov.github.io
  * Text Domain:     db-emojis-auto-replacer
  * Domain Path:     /languages
  * Version:         0.1.0
