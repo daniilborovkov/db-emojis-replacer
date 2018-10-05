740fda34837af080692fd4adbb16cc4f80a60df4
daniilborovkov <daniilborowkow@gmail.com>
2018-10-05 17:37:16 +0600

    Fixed editions

--- db-emojis-auto-replacer.php
+++ db-emojis-auto-replacer.php
@@ -14,7 +14,7 @@
 
 require plugin_dir_path(__FILE__) . 'shortcode.php';
 require plugin_dir_path(__FILE__) . 'scripts.php';
-// require plugin_dir_path(__FILE__) . 'admin-page.php';
+require plugin_dir_path(__FILE__) . 'admin-page.php';
 require plugin_dir_path(__FILE__) . 'rest-api.php';
 
 register_activation_hook(__FILE__, 'db_emojis_auto_replacer_activator');
@@ -39,27 +39,35 @@ function db_emojis_auto_replacer_activator()
     require_once ABSPATH . 'wp-admin/includes/upgrade.php';
     dbDelta($sql);
     // 2. seed from csv file
+    // 2. seed from csv file
     $file_name = dirname(__FILE__) . '/emojis.csv';
     ini_set('auto_detect_line_endings', true);
     $handle = fopen($file_name, 'r');
     // $data   = fgetcsv($handle);
     $row                 = 1;
-    $emojis_insert_query = "INSERT INTO " . $table_name . "(emoji, replacement) VALUES (";
+    $emojis_insert_query = "INSERT INTO " . $table_name . "(emoji, replacement) VALUES ";
+    $values = array();
+    $place_holders = array();
     while (($data = fgetcsv($handle, 1000, ",")) !== false) {
         $replacement = strtolower($data[1]);
+        $replacement = addslashes($replacement);
 
         $search         = "SELECT * FROM $table_name WHERE replacement = '$replacement'";
         $search_results = $wpdb->get_results($search);
 
         if (count($search_results) === 0) {
             // $insert  = "INSERT INTO " . $table_name . "(emoji, replacement) VALUES ('" . $data[0] . "', '" . $replacement . "')";
-            $emojis_insert_query .= ", ('" . $data[0] . "', '" . $replacement . "')";
+            // $emojis_insert_query .= "('" . $data[0] . "', '" . $replacement . "')";
+            array_push($values, $data[0], $replacement);
+            $place_holders[] = "('%s', '%s')";
         }
 
         $row++;
     }
-    $emojis_insert_query .= "')";
-    $results = $wpdb->query($emojis_insert_query);
+
+    $emojis_insert_query .= implode(', ', $place_holders);
+
+    $results = $wpdb->query($wpdb->prepare("$emojis_insert_query ", $values));
     fclose($handle);
     create_folder($table_name);
 }
